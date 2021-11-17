2c86d7218a07c6061ad38e9832b002a72efcb207
Afiery <56961591+Afiery@users.noreply.github.com>
2021-11-17 09:34:16 +0800

    Model Audit Updated

--- app/Models/Audit.php
+++ app/Models/Audit.php
@@ -12,6 +12,6 @@ class Audit extends Model
 {
     protected $table = 'laporan_audit';
     protected $primaryKey = 'no_audit';
-    protected $fillable = ['no_audit','no_laporan_audit','tipe_audit','jenis_audit','objek','auditor','department','kriteria_audit','tahun_audit','tanggal_mulai_audit','tanggal_akhir_audit','jumlah_temuan','file'];
+    protected $fillable = ['no_audit','no_laporan_audit','judul_audit','status_audit','percentage_audit','tipe_audit','jenis_audit','objek','auditor','department','kriteria_audit','tahun_audit','tanggal_mulai_audit','tanggal_akhir_audit','jumlah_temuan','root_audit','corrective_action','file'];
 
 }
