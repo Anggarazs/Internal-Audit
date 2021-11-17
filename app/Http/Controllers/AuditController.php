bcdc6006af7a866f151f5114feba38c008ba2b85
Afiery <56961591+Afiery@users.noreply.github.com>
2021-11-17 09:34:58 +0800

    AuditController Updated

--- app/Http/Controllers/AuditController.php
+++ app/Http/Controllers/AuditController.php
@@ -19,17 +19,11 @@ class AuditController extends Controller
     }
 
     public function insert_audit(Request $request){
-       // Andi Implementation
-    //     $string = "";
-    //     $new_var = $request->jenis_audit;
-    // foreach ($new_var as $key => $value) {
-    //     $jenis1 = JenisAudit::where('id_jenis_audit',$value)->first();
-    //     $string =$string.$jenis1->jenis_audit.', ';
-    // }
-    //     echo $string;
+
         $validateData = $request->validate([
             "no_laporan_audit" => 'required|max:255',
             "judul_audit" => 'required|max:255',
+            "status_audit" => 'required|max:255',
             "tipe_audit" => 'required|max:255',
             "jenis_audit" => 'required|max:255',
             "objek" => 'required|max:255',
@@ -39,13 +33,8 @@ class AuditController extends Controller
             "tahun_audit" => 'required',
             "tanggal_mulai_audit" => 'required|date_format:Y-m-d',
             "tanggal_akhir_audit" => 'required|date_format:Y-m-d',
-            "jumlah_temuan" => 'required',
             "file" => 'mimes:pdf,jpg,jpeg,png|max:10000'
         ]);
-     
-        // $jenis1 = JenisAudit::where('id_jenis_audit','1')->first();
-        // $jenis2 = JenisAudit::where('id_jenis_audit','2')->first();
-        // $string = $jenis1->jenis_audit.', '.$jenis2->jenis_audit;
 
         $dokumen=$request->judul_audit.'.'.$request->file->extension();
         $request->file->move(storage_path('app/public/LaporanAudit'),$dokumen);
@@ -53,6 +42,8 @@ class AuditController extends Controller
         $audit=new Audit;
         $audit->no_laporan_audit=$request->get('no_laporan_audit');
         $audit->judul_audit=$request->get('judul_audit');
+        $audit->status_audit=$request->get('status_audit');
+        $audit->percentage_audit='0%';
         $audit->tipe_audit=$request->get('tipe_audit');
         $audit->jenis_audit=$request->get('jenis_audit');
         $audit->objek=$request->get('objek');
@@ -63,38 +54,14 @@ class AuditController extends Controller
         $audit->tanggal_mulai_audit=$request->get('tanggal_mulai_audit');
         $audit->tanggal_akhir_audit=$request->get('tanggal_akhir_audit');
         $audit->jumlah_temuan=$request->get('jumlah_temuan');
+        $audit->root_audit='0';
+        $audit->corrective_action='0';
         $audit->file=$dokumen;
-        // $audit->jenis_audit()->attach($request->jenis_audit);
         $audit->save();
 
-        session()->flash('berhasil', 'Laporan Audit Berhasil Ditambahkan');
+        session()->flash('berhasil', 'Event Audit Berhasil Ditambahkan');
         return redirect('/audit');
         
-
-          // $audit=Audit::create([
-        //     'no_laporan_audit'=>$request->no_laporan_audit,
-        //     'judul_audit'=>$request->judul_audit,
-        //     'tipe_audit'=>$request->tipe_audit,
-        //     'objek'=>$request->objek,
-        //     'auditor'=>$request->auditor,
-        //     'department'=>$request->department,
-        //     'kriteria_audit'=>$request->kriteria_audit,
-        //     'tahun_audit'=>$request->tahun_audit,
-        //     'tanggal_mulai_audit'=>$request->tanggal_mulai_audit,
-        //     'tanggal_akhir_audit'=>$request->tanggal_akhir_audit,
-        //     'jumlah_temuan'=>$request->jumlah_temuan,
-        //     'file'=>$dokumen
-        // ]);
-
-        // $jenis_audit=$request->jenis_audit;
-        // $audit->jenis_audit()->sync($jenis_audit);
-   
-      
-
-        // if ($audit->save()) {
-        //     $jenis->jenis_audit=$request->get('jenis_audit');
-        //     $audit->jenis_audit()->sync($jenis);
-        // }
     }
 
     public function edit_audit($id){
@@ -108,12 +75,15 @@ class AuditController extends Controller
     public function edit_audit_process(Request $request){
         $audit = Audit::where('no_audit', $request['no_audit']);
         $dokumen=$request->judul_audit.'.'.$request->file->extension();
-        
+        // $file_data_laporan_audit = $request->file;
+        // $fileName_data_laporan_audit = time().'_'.$file_data_laporan_audit->getClientOriginalName();
+        // $file_data_laporan_audit->move(public_path('storage/file-peminjaman/data-diri-penanggungjawab'), $fileName_data_diri_penanggungjawab);
 
         $data = [
             'no_laporan_audit' => $request['no_laporan_audit'],
             'judul_audit' =>  $request['judul_audit'],
-            'tipe_audit' =>  $request['tipe_audit'],
+            'status_audit' =>  $request['status_audit'],
+            'percentage_audit' =>  $request['percentage_audit'],
             'jenis_audit' =>  $request['jenis_audit'],
             'objek' =>  $request['objek'],
             'auditor' => $request['auditor'],
@@ -127,49 +97,17 @@ class AuditController extends Controller
 
         $request->file->move(storage_path('app/public/LaporanAudit'),$dokumen);
         $audit->update($data);
-        session()->flash('berhasil', 'Laporan Audit Berhasil Diubah');
+        session()->flash('berhasil', 'Event Audit Berhasil Diubah');
         return redirect('/audit');
 
+        
 
     }
-    //     $validateData = $request->validate([
-    //         "no_laporan_audit" => 'required|max:255',
-    //         "judul_audit" => 'required|max:255',
-    //         "tipe_audit" => 'required|max:255',
-    //         "jenis_audit" => 'required|max:255',
-    //         "objek" => 'required|max:255',
-    //         "auditor" => 'required|max:255',
-    //         "department" => 'required',
-    //         "kriteria_audit" => 'required|max:255',
-    //         "tahun_audit" => 'required',
-    //         "tanggal_mulai_audit" => 'required|date_format:Y-m-d',
-    //         "tanggal_akhir_audit" => 'required|date_format:Y-m-d',
-    //         "jumlah_temuan" => 'required',
-    //         "file" => 'mimes:pdf,jpg,jpeg,png|max:10000'
-    //     ]);
-    //     $dokumen=$request->judul_audit.'.'.$request->file->extension();
-    //     $request->file->move(storage_path('app/public/LaporanAudit'),$dokumen);
-        
-    //     if ($validateData) {
-    //         $audit = Audit::where('no_audit', $request['no_audit'])
-    //         ->update(['no_laporan_audit' => $request["no_laporan_audit"], 'judul_audit' => $request["judul_audit"], 'tipe_audit' => $request["tipe_audit"], 'jenis_audit' => $request["jenis_audit"], 'objek' => $request["objek"],
-    //         'auditor' => $request["auditor"],'department' => $request["department"],'kriteria_audit' => $request["kriteria_audit"],'tahun_audit' => $request["tahun_audit"],'tanggal_mulai_audit' => $request["tanggal_mulai_audit"],
-    //         'tanggal_akhir_audit' => $request["tanggal_akhir_audit"],'jumlah_temuan' => $request["jumlah_temuan"],'file' => $request["file"]]);
-
-    //         if ($audit) {
-    //             session()->flash('berhasil', 'Akun Berhasil Diubah');
-    //             return redirect('/audit');
-    //         } else {
-    //             session()->flash('gagal', 'Akun Gagal Diubah');
-    //             return redirect('/edit_audit/' . $request["no_audit"]);
-    //         }
-    //     }
-    // }
 
     public function delete_audit($id){
         $audit =Audit::where("no_audit", $id);
         $audit->delete();
-        session()->flash('berhasil', 'Laporan Audit Berhasil Dihapus');
+        session()->flash('berhasil', 'Event Audit Berhasil Dihapus');
         return redirect('/audit');
     }
 }
