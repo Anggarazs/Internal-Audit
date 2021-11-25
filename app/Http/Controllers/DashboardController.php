c914732250f669b945c437cb5cb36968b5d1c713
Afiery <56961591+Afiery@users.noreply.github.com>
2021-11-25 13:56:54 +0800

    Update DashboardController.php

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
 
 class DashboardController extends Controller
 {
     public function index(){
-        return view('dashboard.index');
+        $laporan_audit = DB::table('laporan_audit')->count();
+        $event_audit = DB::table('corrective_action')->count();
+        return view('dashboard.index',compact('laporan_audit','event_audit'));
     }
 }
