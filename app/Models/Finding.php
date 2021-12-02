ed6aa465fc5e1f2f89bb0986eee61166b2f666a4
Afiery <56961591+Afiery@users.noreply.github.com>
2021-12-02 14:42:18 +0800

    Update Finding.php

--- app/Models/Finding.php
+++ app/Models/Finding.php
@@ -10,7 +10,7 @@ class Finding extends Model
     use HasFactory;
     protected $table = 'finding';
     protected $primaryKey = 'id_finding';
-    protected $fillable = ['id_finding','no_audit','no_laporan_audit','judul_audit','progress','tipe_audit','jenis_audit','kriteria_audit','tahun_audit','tanggal_mulai_audit','tanggal_akhir_audit','auditor','finding','root','department','auditee','corrective','fu_corrective','comment','due_date','file'];
+    protected $fillable = ['id_finding','no_audit','no_laporan_audit','judul_audit','tipe_audit','jenis_audit','kriteria_audit','tahun_audit','tanggal_mulai_audit','tanggal_akhir_audit','auditor','finding','root','department','auditee','corrective','fu_corrective','comment','due_date','file'];
 
 
 }
